18f685a0dc89e1c86e5ff6e9cb53ea695b728d51
Elton Baidoo <146414760+eltonbaidoo@users.noreply.github.com>
2025-02-11 08:30:59 -0500

    Update DocToTxt.py

--- DocToTxt.py
+++ DocToTxt.py
@@ -1,9 +1,12 @@
 from docx import Document
 
 # Load the .docx file
-doc = Document('HW1 Introduction 2.docx')
+doc = Document('Name of File.docx')
 #replace the file name with the file you want to convert
+#Inlcude the path in it does not have a file 
 
 # Extract and print the text
+option = int(input("1. Print in Console \n 2. Export to Txt file "))
+
 for paragraph in doc.paragraphs:
     print(paragraph.text)
